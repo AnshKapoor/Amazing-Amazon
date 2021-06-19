d4ed12591c38e3813d3c8057e101aef3413438de
Ansh Kapoor <ansh605@gmail.com>
2021-06-20 02:08:22 +0530

    Product component and it's css

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 function Home() {
   return (
     <div className="home">
@@ -9,6 +10,9 @@ function Home() {
           src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/Gateway/WRS-Jun/Teaser_graphics/PC_Teaser_graphics/Rev/teaser_PC_Bunk_PEA_50._CB666229056_.jpg"
           alt="amazon-banner"
         />
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
     </div>
   );
